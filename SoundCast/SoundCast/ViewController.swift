774c658a3efec92ef15c4ebdd4fc8212904aae2c
DJ <>
2018-08-06 01:09:23 +0530

    Added cocoapods. Added Alamofire and AlamofireImage frameworks.

---
+++ SoundCast/SoundCast/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoundCast
+//
+//  Created by Dhananjay Kumar Dubey on 05/08/18.
+//  Copyright © 2018 Dhananjay Kumar Dubey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
